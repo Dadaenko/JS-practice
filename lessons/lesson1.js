303c21073cbe166f4a9e60735811aa3c322920e8
Daria Makarenko <darya.makarenko@gmail.com>
2026-04-27 22:07:52 +0300

    variables types lesson

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,2 +1,26 @@
 //1. Hello World
 console.log("Hello World!")
+
+//2. Variable
+var firstName = "Janny"
+let lastName = "Doe"
+console.log(lastName)
+
+var age, dateOfBirth, sex 
+age = "25"
+sex = "Female"
+console.log(age)
+age = "35"
+console.log(age)
+
+//3. Constants
+const profession = "Artist"
+console.log(profession)
+
+//4. Data types
+
+var middleName = "Marie" //string
+var ageOfBorther = 41 //number (when "" or '' - not able to perform mathematical operations, seen as a string)
+var isMarried = false //boolean (used for logical operations)
+var yearsInMarriage = null //null (no value) 
+var numberOfCars = undefined //undefined (seen usually in error states, cannot use something that doesn't exist)
