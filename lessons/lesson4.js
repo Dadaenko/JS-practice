76eefe70b3e727a6deca85150f476285cf3da5db
Daria Makarenko <darya.makarenko@gmail.com>
2026-04-29 22:13:16 +0300

    Operators lesson

---
+++ lessons/lesson4.js
@@ -0,0 +1,15 @@
+//Relational or comparison operators
+
+// > - more than
+// < - less than
+// >= - more than equal
+// <= - less than equal 
+
+var result = 10 <= 5
+console.log(result)
+
+//Equality operators
+
+var x = 1
+console.log(x=='1') //true - loose comparison, value on both sides are compared
+console.log(x==='1') //false - strict comparison, not only value compared, but also the data type (integer and string are not equal)
