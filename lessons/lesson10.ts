d48856b1bfe579531602938404c3dc6e2ddf742b
Daria Makarenko <darya.makarenko@gmail.com>
2026-06-10 22:18:54 +0300

    TS vs JS lesson

---
+++ lessons/lesson10.ts
@@ -0,0 +1,12 @@
+
+var customerFirstName: string = "Diana"
+var customerLastName: string = "Woodraw"
+var customerAge: number = 40
+
+type Customer = {firstName: string, lastName: string, active: boolean}
+
+var firstCustomer: Customer = {
+    firstName: "Bella",
+    lastName: "Brenson",
+    active: true
+}
